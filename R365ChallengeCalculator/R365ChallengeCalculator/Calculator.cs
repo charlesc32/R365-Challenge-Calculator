9cf837f0efa581a594ccd29e1a851d69970d6dfc
Charles Cymbor <charles.cymbor@autoalert.com>
2019-10-07 11:06:59 -0700

    Requirement 4

--- R365ChallengeCalculator/R365ChallengeCalculator/Calculator.cs
+++ R365ChallengeCalculator/R365ChallengeCalculator/Calculator.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace R365ChallengeCalculator
 {
     public class Calculator: ICalculator
     {
+        private static readonly int LOWERBOUND = 0;
         public int Calculate(string[] inputs)
         {
             int sum = 0;
+            var deniedNegativeNumbers = new List<int>();
 
             foreach (string inputItem in inputs)
             {
@@ -15,7 +18,18 @@ namespace R365ChallengeCalculator
                     input = 0;
                 }
 
+                if (input < 0)
+                {
+                    deniedNegativeNumbers.Add(input);
+                    continue;
+                }
+                                
                 sum += input;
+            }
+
+            if (deniedNegativeNumbers.Count > 0)
+            {
+                throw new InvalidOperationException($"Negative numbers are not allowed. Negative numbers detected: {string.Join(',', deniedNegativeNumbers)}");
             }
 
             return sum;
--- R365ChallengeCalculator/R365ChallengeCalculatorTests/CalculatorTests.cs
+++ R365ChallengeCalculator/R365ChallengeCalculatorTests/CalculatorTests.cs
@@ -12,14 +12,20 @@ namespace R365ChallengeCalculatorTests
         [DataRow(new string[] { "1", "xxx" }, 1)]
         [DataRow(new string[] { "20" }, 20)]
         [DataRow(new string[] { "1", "5000" }, 5001)]
-        [DataRow(new string[] { "4", "-3" }, 1)]
-        [DataRow(new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" }, 78)]
-         
+        [DataRow(new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" }, 78)]         
         public void Calculate_ShouldSucceed(string[] input, int expectedSum)
         {
             var calculator = new Calculator();
             int result = calculator.Calculate(input);
             Assert.AreEqual(expectedSum, result);
+        }
+
+        [TestMethod]
+        public void Calculate_ShouldThrowWithNegativeNumbers()
+        {
+            var calculator = new Calculator();
+            var exception = Assert.ThrowsException<InvalidOperationException>(() => calculator.Calculate(new string[] { "-1", "-2"}));
+            Console.WriteLine(exception.Message);
         }
     }
 }
